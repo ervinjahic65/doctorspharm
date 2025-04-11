f527573a6ce55c49a217f9f42c8563883edd726b
ervinjahic65 <ervinjahic65@gmail.com>
2025-04-12 01:06:46 +0200

    add id

---
+++ id/files/custom.js
@@ -0,0 +1,105 @@
+jQuery(document).ready(function () {
+
+
+    $('#carouselHacked').carousel();
+
+    //this code is for the gmap
+    
+
+
+    //this code is for smooth scroll and nav selector
+    $(document).ready(function () {
+        $(document).on("scroll", onScroll);
+
+        //smoothscroll
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+
+            $('a').each(function () {
+                $(this).removeClass('active');
+            })
+            $(this).addClass('active');
+
+            var target = this.hash,
+                menu = target;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top + 2
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+    });
+
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+        $('.navbar-default .navbar-nav>li>a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+        });
+    }
+
+
+    //this code is for animation nav
+    jQuery(window).scroll(function () {
+        var windowScrollPosTop = jQuery(window).scrollTop();
+
+        if (windowScrollPosTop >= 150) {
+            jQuery(".header").css({
+                "background": "#B193DD",
+            });
+            jQuery(".top-header img.logo").css({
+                "margin-top": "-40px",
+                "margin-bottom": "0"
+            });
+            jQuery(".navbar-default").css({
+                "margin-top": "-15px",
+            });
+        } else {
+            jQuery(".header").css({
+                "background": "transparent",
+            });
+            jQuery(".top-header img.logo").css({
+                "margin-top": "-15px",
+                "margin-bottom": "25px"
+            });
+            jQuery(".navbar-default").css({
+                "margin-top": "12px",
+                "margin-bottom": "0"
+            });
+
+        }
+    });
+
+
+
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const faqItems = document.querySelectorAll('.faq-item');
+    
+    faqItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const isActive = this.classList.contains('active');
+            const icon = this.querySelector('i.fa-chevron-down');
+            const content = this.querySelector('.faq-content');
+            
+            // Close all items first
+            faqItems.forEach(el => {
+                el.classList.remove('active');
+                el.querySelector('.faq-content').style.display = 'none';
+                el.querySelector('i.fa-chevron-down').style.transform = 'translateX(0) rotate(0)';
+            });
+            
+            // If clicked item wasn't active, open it
+            if (!isActive) {
+                this.classList.add('active');
+                content.style.display = 'block';
+                icon.style.transform = 'translateX(5px) rotate(180deg)';
+            }
+        });
+    });
+});
